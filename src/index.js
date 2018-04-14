0f5029bf46ff74eec7e949c3fe168ef6f67b43e0
andreas.groos <andreas.groos1@gmail.com>
2018-04-13 23:25:02 -0700

    added jsdoc jsdoc.config.json, create-docs script in package.json

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
 import chalk from "chalk";
 import { stations, loadedStations } from "./data";
-console.log(chalk.green("Working"));
-
-async function test() {
-  await console.log("async");
-}
-
-test();
